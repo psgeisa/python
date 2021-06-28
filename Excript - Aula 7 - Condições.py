36dd31b27c227ed826ca057b6ad62857831d4122
Geisa Reis <72582427+psgeisa@users.noreply.github.com>
2021-06-28 00:10:28 -0300

    Delete Excript - Aula 7 - Condições.py

--- Excript - Aula 7 - Condições.py
+++
@@ -1,8 +0,0 @@
-acao = int(input("Digite '1' para sim e '2' para não: "))
-if (acao == 1):
-    print("você disse que sim")
-else:
-    if (acao == 2):
-         print("você disse que não")
-    else:
-        print("o número digitado não é '1' nem '2'")
