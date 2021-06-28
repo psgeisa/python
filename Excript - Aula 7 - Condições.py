c64db7048edbcfa88b504ee08f96d1c139ddba57
Geisa Reis <72582427+psgeisa@users.noreply.github.com>
2021-06-27 23:58:14 -0300

    #condições

---
+++ Excript - Aula 7 - Condições.py
@@ -0,0 +1,8 @@
+acao = int(input("Digite '1' para sim e '2' para não: "))
+if (acao == 1):
+    print("você disse que sim")
+else:
+    if (acao == 2):
+         print("você disse que não")
+    else:
+        print("o número digitado não é '1' nem '2'")
