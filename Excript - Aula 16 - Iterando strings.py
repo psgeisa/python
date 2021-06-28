eb9b7090aa62d57df7503f5e59ea2810ed09eb6f
Geisa Reis <72582427+psgeisa@users.noreply.github.com>
2021-06-28 00:18:14 -0300

    #enumerate

--- Excript - Aula 16 - Iterando strings.py
+++ Excript - Aula 16 - Iterando strings.py
@@ -12,8 +12,8 @@ indice = 0
 while indice < len(s):
     print(indice,s[indice]) #valor do índice e carectere respectivo
     indice+=1
-
 """
+
 #Exemplo 3 - a função enumerate tem como carcterística principal: vincular objeto a numeros int não negativos
 for k,v in enumerate("Iterando strings"):
     print(k,v)
