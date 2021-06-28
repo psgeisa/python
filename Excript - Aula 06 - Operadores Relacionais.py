a965710566c14c3af5d59b77bf33db733ce93c98
Geisa Reis <72582427+psgeisa@users.noreply.github.com>
2021-06-28 00:14:59 -0300

    #ops_relacionais

--- Excript - Aula 06 - Operadores Relacionais.py
+++ Excript - Aula 06 - Operadores Relacionais.py
@@ -1,4 +1,4 @@
-#Operadores de igualdade
+# Operadores de igualdade
 print(10 == 10) #É verdade que os valores são iguais
 print(10 == 11) #É falso que os valores são iguais
 print(10 != 10) #É falso que os valores são diferentes
