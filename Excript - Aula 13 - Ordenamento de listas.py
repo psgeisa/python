00064cfd5bc6f078e451d1bc5d20b15a08173941
Geisa Reis <72582427+psgeisa@users.noreply.github.com>
2021-06-28 00:16:56 -0300

    #sort

--- Excript - Aula 13 - Ordenamento de listas.py
+++ Excript - Aula 13 - Ordenamento de listas.py
@@ -1,10 +1,10 @@
 lista = ["Geisa", "Gisele", "Bruno", "Gilberto", "Thazila"]
 
-lista.reverse() #inverte o print da lista
+lista.reverse() # inverte o print da lista
 print(lista)
 
-lista.sort() #ordena em ordem alfabética
+lista.sort() # ordena em ordem alfabética
 print(lista)
 
-lista.sort(reverse=True) #ordena em ordem alfabética de ZaA
+lista.sort(reverse=True) # ordena em ordem alfabética de ZaA
 print(lista)
