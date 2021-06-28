64024285ec294ab139adb9cf9c9148707d6a48ee
Geisa Reis <72582427+psgeisa@users.noreply.github.com>
2021-06-28 00:20:02 -0300

    #variadicas

--- Excript - Aula 20 - Funções Variádicas.py
+++ Excript - Aula 20 - Funções Variádicas.py
@@ -6,8 +6,8 @@ for pessoa, numeros in numeros_favoritos.items():
 O interpretador do py utiliza o "+" acompanhado de "str" para alguns casos específicos...
 tipo "print(texto + str(numero))" então ele transforma tudo que está dentro do parâmetro
 em "string" pra poder executar o print
-
 """
+
 numeros_favoritos = {'pietro': [18, 9], 'katia': [7, 14], 'tiago': [6, 4]}
 for pessoa, numeros in numeros_favoritos.items():
     print("O número favorito de", pessoa.title(), " é " , numeros)
